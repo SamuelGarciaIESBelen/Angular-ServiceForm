adde7cc3cf17b6b46d7efa3602ac6ef8e3c1a28f
Samuel <sgarzor433@g.educaand.es>
2025-02-07 10:19:35 +0100

    Primeros pasos

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ServiceForm' title`, () => {
+  it(`should have the 'AngularLogService' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ServiceForm');
+    expect(app.title).toEqual('AngularLogService');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ServiceForm');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularLogService');
   });
 });
